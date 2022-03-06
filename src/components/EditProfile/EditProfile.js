e87dcbac1cf6356424857e7e6c0e044e2d102889
mahmoud <mahmoud.abuattiy106@gmail.com>
2022-03-06 20:23:54 +0200

    uploadcare

--- src/components/EditProfile/EditProfile.js
+++ src/components/EditProfile/EditProfile.js
@@ -1,9 +1,40 @@
 import React, { Component } from "react";
 import "./EditProfile.scss";
 import ProfileImg from "../../images/profile.jpg";
+// import { Widget } from "@uploadcare/react-widget";
+// import UPLOADCARE_PUBLIC_KEY from "@uploadcare/react-widget"
 
 export default class EditProfile extends Component {
   render() {
+    ////////////////////////////////////////////////////
+    // Getting an instance of the widget.
+    // const widget = uploadcare.Widget('[role=uploadcare-uploader]');
+    // // Selecting an image to be replaced with the uploaded one.
+    // const preview = document.getElementById('photo');
+    // // "onUploadComplete" lets you get file info once it has been uploaded.
+    // // "cdnUrl" holds a URL of the uploaded file: to replace a preview with.
+    // let info;
+
+    // widget.onUploadComplete(fileInfo => {
+    //   info = fileInfo;
+    //   document.getElementById("id_hidden_image").value = fileInfo.cdnUrl
+    //   preview.src = fileInfo.cdnUrl;
+    // });
+
+    // let icon = document.querySelector(".edit-icon");
+    // let btn = document.querySelector(".uploadcare--widget__button_type_open");
+    // btn.style.display = 'hidden';
+    // icon.addEventListener("click", () => {
+    //   btn.click();
+    // })
+    // const editIMG = () => {
+    //   console.log(
+    //     document.querySelector(
+    //       ".uploadcare--widget__button uploadcare--widget__button_type_open"
+    //     )
+    //   );
+    // };
+    ////////////////////////////////////////////////////
     const subAni = () => {
       let loader = document.querySelector(".loader"),
         check = document.querySelector(".check");
@@ -18,10 +49,23 @@ export default class EditProfile extends Component {
           <div className="container">
             <div className="info">
               <img
+              id="photo"
                 className="profile-image"
                 src={ProfileImg}
                 alt="profile"
-              ></img><div className="edit-photo"><i className="fa-solid fa-pen-to-square"></i></div>
+              ></img>
+              <div className="edit-photo">
+                {/* <Widget publicKey="YOUR_PUBLIC_KEY" />; */}
+                <div className="crop-img">
+                  <input
+                    type="hidden"
+                    role="uploadcare-uploader"
+                    data-crop="1:1"
+                    data-images-only
+                  />
+                </div>
+                <i className="fa-solid fa-pen-to-square"></i>
+              </div>
               <h2>Mahmoud Abu-Attiya</h2>
             </div>
             <div className="share">
@@ -70,22 +114,6 @@ export default class EditProfile extends Component {
               />
               <label htmlFor="floatingInput">Email address</label>
             </div>
-            <div className="form-floating mb-3">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Password"
-              />
-              <label htmlFor="floatingInput">Password</label>
-            </div>
-            <div className="form-floating">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Password"
-              />
-              <label htmlFor="floatingPassword">Password</label>
-            </div>
           </div>
 
           <div className="main">
@@ -100,7 +128,18 @@ export default class EditProfile extends Component {
             </div>
           </div>
 
-          <a href="/">log out <i className="fa-solid fa-arrow-right-from-bracket"></i></a>
+          <a href="/">
+            log out <i className="fa-solid fa-arrow-right-from-bracket"></i>
+          </a>
+          {/* crop image */}
+          {/* <div className="crop-img">
+            <script>
+              {UPLOADCARE_PUBLIC_KEY = "demopublickey"}
+            </script>
+            <script src="https://ucarecdn.com/libs/widget/3.x/uploadcare.full.min.js" charset="utf-8"></script>
+            <input type="hidden" role="uploadcare-uploader" data-crop="1:1" data-images-only />
+          </div> */}
+          {/* crop image */}
         </div>
       </div>
     );
